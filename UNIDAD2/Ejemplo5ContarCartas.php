30a9a996f7af2fdd08e19f9d99db8c9f53afc3c5
blame110 <46011863+blame110@users.noreply.github.com>
2023-10-19 14:11:19 +0200

    19 oct

--- UNIDAD2/Ejemplo5ContarCartas.php
+++ UNIDAD2/Ejemplo5ContarCartas.php
@@ -13,7 +13,11 @@ if (isset($_POST["lista_cartas"])) {
 
     foreach ($lista_cartas as $carta) {
         //Sacamos la ultima posicion de la carta que es el palo
-        $palo = substr($carta, 2, 2);
+        //Si el palo es mayor de 3 posiciones la ultima donde esta el palo es la 3
+        if (strlen($carta) > 3)
+            $palo = substr($carta, 3, 3);
+        else
+            $palo = substr($carta, 2, 2);
 
         //incrementamos en el array asociativog el palo de la carta actual
         $cantidad_palo[$nombre_palo[$palo]]++;
@@ -25,6 +29,6 @@ if (isset($_POST["lista_cartas"])) {
     var_dump($cantidad_palo);
 
     for ($i = 0; $i < count($cantidad_palo); $i++) {
-        echo "Hay ".$cantidad_palo[$nombre_palo[$i]]." cartas del palo $nombre_palo[$i] <br/>";
+        echo "Hay " . $cantidad_palo[$nombre_palo[$i]] . " cartas del palo $nombre_palo[$i] <br/>";
     }
 }
