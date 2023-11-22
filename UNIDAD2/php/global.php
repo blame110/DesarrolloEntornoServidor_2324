a48230c67681232a952acf9c44b6c8e582793f2f
blame110 <46011863+blame110@users.noreply.github.com>
2023-11-22 11:24:20 +0100

    EXAMEN UT2 RESUELTO

--- UNIDAD2/php/global.php
+++ UNIDAD2/php/global.php
@@ -4,4 +4,7 @@
 $nombre_palo =["Corazones","Picas","Treboles","Diamantes"];
 $nombre_carta =["As","Dos","Tres","Cuatro","Cinco","Seis","Siete","Ocho","Nueve","Diez","J","Q","K"];
 
+const MAX_HP =2000;
+
+
 ?>
