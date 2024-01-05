45f817ef331977d41ef2008ba76083cddc9629bf
blame110 <46011863+blame110@users.noreply.github.com>
2024-01-05 16:46:34 +0100

    Final Navidad

--- UNIDAD3/PINPON/model/Producto.php
+++ UNIDAD3/PINPON/model/Producto.php
@@ -1,155 +1,37 @@
 <?php 
+namespace PINPON\model;
+use PDOException;
 
-class Producto
+class Producto {
+
+
+/**
+ * Devuelve un array asociativo con todos los datos
+ * de la tabla productos
+ */
+public static function getProductos($pdo)
 {
 
-    //Si utilizamos private solo se puede utilizar desde dentro de la clase
+    try {
+        //Realizamos una query
+    $query = "SELECT * FROM productos";
 
-    //Protected implica que se puede acceder a ella solo desde su propia clase
-    //y sus clases hijas
-    private $id;
-    private $nombre;
-    private $descripcion;
-    private $peso;
-    private $precio;
-    private $tamanio;
+    $resultado = $pdo->query($query);
 
-    //Las variables estaticas se utilizan para compartir valores entre todos los objetos
-    public static $cantidad=0;
+    //FetchAll nos saca todos los registros de la query
+    //El fetchall no se puede utilizar mas de una vez
+    $resulSet = $resultado->fetchAll();
 
-    //declaramos estaticos los metodos que podamos utilizar
-    //sin necesidad de utilizar un objeto de este tipo
-    public static function incrementarCantidad()
-    {
-        self::$cantidad++;
-    }
-
-    /**
-     * Constructor de la clase
-     */
-    public function __construct($id, $nombre,$descripcion,$peso,$precio,$tamanio)
-    {
-        //Cada vez que se crea un objeto incrementamos el contador de objetos de este tipo
-        self::$cantidad++;
-
-       if (isset($id)) $this->id=$id; else $this->id = 0;
-       if (isset($nombre)) $this->nombre=$nombre; else $this->nombre = "";
-       if (isset($descripcion)) $this->descripcion=$descripcion; else $this->descripcion="";
-       if (isset($peso)) $this->peso=$peso; else $this->peso = random_int(1,5);
-       if (isset($precio)) $this->precio=$precio; else$this->precio= random_int(1,5);
-       if (isset($tamanio)) $this->tamanio=$tamanio; else$this->tamanio = random_int(1,5);
-    }
-
-
-
-    public function get_id() :int 
-    {
-        return $this->id;
+        
+    } catch (PDOException $e) {
+        print "¡Error!: " . $e->getMessage() . "<br/>";
+        die();
     } 
-
-
-    /**
-     * Get the value of nombre
-     */ 
-    public function getNombre()
-    {
-        return $this->nombre;
-    }
-
-    /**
-     * Set the value of nombre
-     *
-     * @return  self
-     */ 
-    public function setNombre($nombre)
-    {
-        $this->nombre = $nombre;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of descripcion
-     */ 
-    public function getDescripcion()
-    {
-        return $this->descripcion;
-    }
-
-    /**
-     * Set the value of descripcion
-     *
-     * @return  self
-     */ 
-    public function setDescripcion($descripcion)
-    {
-        $this->descripcion = $descripcion;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of peso
-     */ 
-    public function getPeso()
-    {
-        return $this->peso;
-    }
-
-    /**
-     * Set the value of peso
-     *
-     * @return  self
-     */ 
-    public function setPeso($peso)
-    {
-        $this->peso = $peso;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of precio
-     */ 
-    public function getPrecio()
-    {
-        return $this->precio;
-    }
-
-    /**
-     * Set the value of precio
-     *
-     * @return  self
-     */ 
-    public function setPrecio($precio)
-    {
-        $this->precio = $precio;
-
-        return $this;
-    }
-    
-
-    /**
-     * Get the value of tamanio
-     */ 
-    public function getTamanio()
-    {
-        return $this->tamanio;
-    }
-
-    /**
-     * Set the value of tamanio
-     *
-     * @return  self
-     */ 
-    public function setTamanio($tamanio)
-    {
-        $this->tamanio = $tamanio;
-
-        return $this;
-    }
+    //Devolvemos los datos de la query
+    return $resulSet;
 }
 
+}
 
 
 ?>
